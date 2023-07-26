dd66352a0ccd2b8cc1c8d765120f832c0955fc81
xojiakbar <xojiakbar@mail.com>
2023-07-26 18:34:57 +0500

    commit

--- app/src/main/java/com/xojiakbar/taskmanager/api/ApiClient.kt
+++ app/src/main/java/com/xojiakbar/taskmanager/api/ApiClient.kt
@@ -1,13 +1,16 @@
 package com.xojiakbar.taskmanager.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ApiClient {
     val  baseUrl = "http://143.198.129.165:8000/api/"
 
-    fun getApiService() : Apiservice
+    fun getApiService() : ApiService
     {
         return Retrofit.Builder().baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(Apiservice::class.java)
+            .create(ApiService::class.java)
     }
 }
